8d3889ba384801f4f87688e294634cd96cbe6602
Aline Ebone <iniebone@gmail.com>
2020-02-20 04:46:45 -0300

    TwoDices App

---
+++ TwoDices/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    
+    let dices: [UIImage] = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstDice.image = dices.randomElement()
+        secondDice.image = dices.randomElement()
+    }
+
+    @IBAction func rollDices(_ sender: Any) {
+        firstDice.image = dices.randomElement()
+        secondDice.image = dices.randomElement()
+    }
+}
